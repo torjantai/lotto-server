b1d75a298662a1bd96c3138a43e0d4a1dbf04027
Antti Lylander <antti.lylander@gmail.com>
2018-06-20 18:34:07 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const lottery = require('./lottery');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+
+// app.get('/', (req, res) => {
+//   res.send('hello');
+// })
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  // res.redirect('/');
+  lottery.play(req.body.drawUntilScore, req.body.howManyWeeks);
+  const scoreCount = lottery.scoreCount;
+  res.send(scoreCount);
+});
+
+app.use(express.static('public'));
+
+
+app.listen(3000, () => console.log('listening on port 3000'));
---
+++ lottery.js
@@ -0,0 +1,73 @@
+'use strict';
+
+let drawUntilScore = 7;
+let scoreCount;
+let howManyWeeks = 1000;
+let weeksDrawn = 0;
+let rowScore = 0;
+
+function consoleLog(printThis, printThat) {
+  console.log(printThis, printThat);
+}
+
+//this function creates two separate rows that can be conpared against each other
+//passes rows to callback
+function createRows(callback) {
+  const rows =  [ [], [] ];
+  let randomBallIndex;
+  let balls = [];
+  for (let i = 0; i < 2; i++) {
+    balls = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
+                  ,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40];
+    for (let j = 0; j < 7; j++) {
+      randomBallIndex = Math.floor(Math.random()*balls.length);
+      rows[i][j] = balls[randomBallIndex];
+      balls.splice(randomBallIndex, 1);
+    }
+  }
+  callback(rows);
+}
+
+//compares the rows and updates scoreCount array accordingly
+function checkRow(rows) {
+  rowScore = 0
+  for ( let i = 0; i < 7; i++ ) {
+    if ( rows[0].indexOf(rows[1][i]) > -1 ) {
+      rowScore++;
+    }
+  }
+  // console.log(rowScore);
+  // console.log(weeksDrawn);
+  weeksDrawn++;
+// if (rowScore < drawUntilScore) {
+  scoreCount[rowScore]++;
+  // }
+}
+
+//runs createRow and checkRow until set conditions are met
+function simulation(callback) {
+  scoreCount = [0,0,0,0,0,0,0,0];
+  weeksDrawn = 0;
+  do {
+    createRows(checkRow);
+  //  console.log(new Date());
+  } while ( rowScore < drawUntilScore && weeksDrawn < howManyWeeks);
+
+  callback(scoreCount, weeksDrawn);
+}
+
+//simulation(consoleLog);
+
+//takes settings from user, runs the simulation and exports scorecount so that it can be accessed in app.js
+function play(theDrawUntilScore, theHowManyWeeks) {
+  drawUntilScore = theDrawUntilScore;
+  howManyWeeks = theHowManyWeeks;
+
+
+  simulation(consoleLog);
+  return module.exports.scoreCount = scoreCount;
+}
+
+//play();
+
+module.exports.play = play;
